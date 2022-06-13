826ba25b02c57b6b1b991454cc676f6add011bf4
Admin <Admin@DESKTOP-3AJ9NM3>
2022-06-13 15:39:23 +0700

    Add project files.

---
+++ lab02/ForEach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab02
+{
+    class ForEach
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Random rand = new Random((int)now.Millisecond);
+            int[] Arr = new int[10];
+            for ( int i = 0; i < Arr.Length; ++i)
+            {
+                Arr[i] = rand.Next() % 100;
+            }
+            int Total = 0;
+            Console.Write("Array values are ");
+            foreach ( int val in Arr)
+                {
+                Total += val;
+                Console.Write(val + " ");
+            }
+            Console.WriteLine("\nAnd the average is {0,0:F1}", (double)Total / (double)Arr.Length);
+            Console.ReadLine();
+        }
+    }
+}
